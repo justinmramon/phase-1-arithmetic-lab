2086feabee7069a07faeb8aa00dd37028b75fc99
Justin M. Ramon <justinmramon@gmail.com>
2022-05-17 10:07:44 -0500

    commit tree e671d90b5f46b1053c4116f4a0a5ab7165fb8d62

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+// function multiply(num1, num2){
+//     console.log(num1*num2)
+//     return (num1 * num2)
+// }
+// multiply(num1, num2)
+
+let random = Math.floor(Math.random() * 10) - 1
+
+let num3 = 68
+let num4 = 8
+
+let mod = num3 % num4
+
+let max = Math.max(5, 10, 15, 20)
